12db66e2eaca5b62be33934f83ca22cf583b479b
mohitjayant <mohitj8790@gmail.com>
2020-08-12 13:43:06 +0530

    Thread implementation using Reuunable interface

--- src/ExampleThread/MyRunnable.java
+++ src/ExampleThread/MyRunnable.java
@@ -1,4 +1,11 @@
 package ExampleThread;
 
-public class MyRunnable {
+import static ExampleThread.ThreadColor.ANSI_RED;
+
+public class MyRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println(ANSI_RED + "Hello from MyRunnable implementation of run()");
+    }
 }
