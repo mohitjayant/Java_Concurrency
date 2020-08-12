fda0b99ab11f2bab834d44774cedc30d76f9ce59
mohitjayant <mohitj8790@gmail.com>
2020-08-12 14:13:44 +0530

    Thread implementation

--- src/ExampleThread/AnotherThread.java
+++ src/ExampleThread/AnotherThread.java
@@ -6,6 +6,15 @@ public class AnotherThread extends Thread {
 
     @Override
     public void run() {
-        System.out.println(ANSI_BLUE+"hey,I am from another Thread");
+        System.out.println(ANSI_BLUE+"hey,I am from " + currentThread().getName());
+
+        try {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e){
+            System.out.println(ANSI_BLUE + "Another thread woke me up");
+        }
+
+        System.out.println(ANSI_BLUE + "Three seconds have passed and I'm awake");
     }
 }
