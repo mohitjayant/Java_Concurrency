799a9e2f1c535ad28b5a381c0adf84e9e7222f38
mohitjayant <mohitj8790@gmail.com>
2020-08-12 13:42:30 +0530

    Thread implementation

--- src/ExampleThread/Main.java
+++ src/ExampleThread/Main.java
@@ -1,4 +1,27 @@
 package ExampleThread;
 
+import static ExampleThread.ThreadColor.ANSI_GREEN;
+import static ExampleThread.ThreadColor.ANSI_PURPLE;
+
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(ANSI_PURPLE+"Hey, I am from main Thread");
+
+        Thread anotherThread=new AnotherThread();
+        anotherThread.start();
+
+/*        new Thread(){
+            public void run(){
+                System.out.println("Hello,From Anonymous class");
+            }
+        }.start();*/
+
+        new Thread(() -> System.out.println(ANSI_GREEN+"Hello,From Anonymous class")).start(); //Anonymous class
+
+        Thread myRunnable=new Thread(new MyRunnable());
+        myRunnable.start();
+
+        System.out.println(ANSI_PURPLE+"Hey, I am again from main Thread");
+    }
 }
