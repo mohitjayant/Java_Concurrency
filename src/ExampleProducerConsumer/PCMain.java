44202d219ff9d2a7340ff59d6241e9ba9b110d48
mohitjayant <mohitj8790@gmail.com>
2020-08-14 05:22:25 +0530

    adding feature of tryLock()

--- src/ExampleProducerConsumer/PCMain.java
+++ src/ExampleProducerConsumer/PCMain.java
@@ -3,6 +3,7 @@ package ExampleProducerConsumer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 import static ExampleProducerConsumer.PCMain.EOF;
@@ -14,13 +15,37 @@ public class PCMain {
     public static void main(String[] args) {
         List<String> buffer=new ArrayList<String>();
         ReentrantLock bufferLock=new ReentrantLock();
+
+        ExecutorService executorService= Executors.newFixedThreadPool(5);
+
         MyProducer producer=new MyProducer(buffer,ThreadColor.ANSI_YELLOW,bufferLock);
         MyConsumer consumer1=new MyConsumer(buffer,ThreadColor.ANSI_PURPLE,bufferLock);
         MyConsumer consumer2=new MyConsumer(buffer,ThreadColor.ANSI_CYAN,bufferLock);
 
-        new Thread(producer).start();
-        new Thread(consumer1).start();
-        new Thread(consumer2).start();
+
+        executorService.execute(producer);
+        executorService.execute(consumer1);
+        executorService.execute(consumer2);
+
+        Future<String> future=executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println(ThreadColor.ANSI_WHITE + "I am being printed from Callable class");
+                return "This is a Callable result";
+            }
+        });
+
+        try {
+            System.out.println(future.get());
+        }
+        catch (ExecutionException e){
+            System.out.println("Something Went Wrong");
+        }
+        catch (InterruptedException e){
+            System.out.println("Thread running the task was interrupted");
+        }
+
+        executorService.shutdown();
     }
 }
 
